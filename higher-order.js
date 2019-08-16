6935d9d9f3a4dabba34cd5897c74eebc399e5d28
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-08-16 15:59:18 -0600

    finished first two problems of higher-order, will finish rest later

--- higher-order.js
+++ higher-order.js
@@ -21,8 +21,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+let evenNumbers = mixedNumbers.filter( function(element, index, wholeArray){
+  if (wholeArray[index] % 2 === 0){
+    return true
+  }
+})
 
 
 ////////// PROBLEM 2 //////////
@@ -43,8 +46,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+
+// let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element, index, wholeArray) {
+  return wholeArray[index] * 1.07
+})
 
 
 
